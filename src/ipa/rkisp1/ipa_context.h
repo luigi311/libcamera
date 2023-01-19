03b225e2fb3776df8c07d165bf5fc4682ab08934
Daniel Semkowicz <dse@thaumatec.com>
2023-01-19 09:41:06 +0100

    rkisp1: Control camera lens position from IPA
    Allow control of lens position from the IPA, by setting corresponding
    af fields in the IPAFrameContext structure. Controls are then passed to
    the pipeline handler, which sets the lens position in CameraLens.
    
    Signed-off-by: Daniel Semkowicz <dse@thaumatec.com>

--- src/ipa/rkisp1/ipa_context.h
+++ src/ipa/rkisp1/ipa_context.h
@@ -63,6 +63,11 @@ struct IPASessionConfiguration {
 
 struct IPAActiveState {
 	struct {
+		uint32_t lensPosition;
+		bool applyLensCtrls;
+	} af;
+
+	struct {
 		struct {
 			uint32_t exposure;
 			double gain;
